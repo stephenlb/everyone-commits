f1dfdfad47c8f1aeb482182932607bdc0c63f156
Stephen Blum <blum.stephen@gmail.com>
2026-07-17 09:03:57 -0700

    Beep Boop

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
+
+## Index Test
 curl http://0.0.0.0:8000/
+
+## Upload Video Test
+VIDEO='{"title":"Beep Boop", "description":"My First Video"}'
+curl -s http://0.0.0.0:8000/video -d $VIDEO -X POST -H "Content-Type: application/json" | jq
